a9a25e4a9c34151a93944d46f4246aa5abfd65a4
brian <ballerbrian4@gmail.com>
2018-05-01 10:21:54 -0700

    added internal score counter with time and points

--- game-sample/main.py
+++ game-sample/main.py
@@ -2,6 +2,7 @@ import pygame
 from global_inst import *
 from bunny import Bunny
 from enemy import Enemy
+import copy
 
 
 def GameLoop():
@@ -11,6 +12,12 @@ def GameLoop():
     pygame.display.set_caption("My game")
     clock = pygame.time.Clock()
 
+    my_font = pygame.font.SysFont("Calibri", 50, True, False)
+    score = 0;
+
+    # score = 0;
+    # scoretext = small_font.render("Score: " + str(score), 1, RED)
+
     player = Bunny()
     ALL_SPRITES.add(player)
     for i in range(8):
@@ -18,9 +25,27 @@ def GameLoop():
         ALL_SPRITES.add(enemy)
         ENEMIES.add(enemy)
 
+    text_player_name = my_font.render("YOU", False, RED)
+
+    ltime=0
+
     running = True
     while running:
-        clock.tick(FPS)
+
+        clock.tick_busy_loop(FPS)
+        #clock.tick(FPS)
+        #ms = clock.get_time()
+        ms = int(pygame.time.get_ticks()/1000)
+
+        #print (ms-ltime)
+
+        if (ms-ltime >=1):
+            score = score + 1
+            print(score)
+
+        ltime = copy.deepcopy(ms)
+
+        # screen.blit(scoretext, (5, 10));
         # Events:
         for event in pygame.event.get():
             # check for closing the window
@@ -36,6 +61,8 @@ def GameLoop():
         hit1 = pygame.sprite.groupcollide(ENEMIES, BULLETS, True, True)
         # if hit, create a new enemy
         for hit in hit1:
+            score = score + 10
+            print("score:"+ str(score))
             enemy = Enemy()
             ALL_SPRITES.add(enemy)
             ENEMIES.add(enemy)
